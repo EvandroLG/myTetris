1fc17186566c9ab30d937ee7f5b5d1561f617148
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-04-01 22:17:28 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const doc = document;
+const canvas = doc.getElementById('game');
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
