d1b70c11677e486f6d0ecf7c464cbadf7ce9adde
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-04-06 20:33:05 +0200

    add logic to update player position

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import updateWrapper from './update';
+import { createMatrix } from './matrix';
 import events from './events';
 
 ('use strict');
@@ -7,7 +8,8 @@ const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 ctx.scale(15, 15);
 
-const update = updateWrapper(ctx, canvas);
+const arena = createMatrix(12, 20);
+const update = updateWrapper(ctx, arena, canvas);
 
 update();
 events();
--- src/update.js
+++ src/update.js
@@ -1,10 +1,11 @@
 import drawMatrix from './drawMatrix';
 import { bg } from './color';
 import player from './player';
+import { isCollision, mergePlayer } from './arena';
 
 ('use strict');
 
-const updateWrapper = (ctx, { width, height }) => {
+const updateWrapper = (ctx, arena, { width, height }) => {
   const draw = () => {
     ctx.fillStyle = bg;
     ctx.fillRect(0, 0, width, height);
@@ -18,6 +19,12 @@ const updateWrapper = (ctx, { width, height }) => {
     if (!last || delta >= 1000) {
       last = time;
       player.position.y++;
+
+      if (isCollision(arena, player)) {
+        player.position.y--;
+        mergePlayer(arena, player);
+        player.position.y = 0;
+      }
     }
 
     draw();
